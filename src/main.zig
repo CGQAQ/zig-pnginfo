f726f9aa6274c37aa6f1468c17c7d55195e5e01b
CGQAQ <m.jason.liu@outlook.com>
2022-11-08 14:53:25 +0800

    fmt & extern

--- src/main.zig
+++ src/main.zig
@@ -6,14 +6,14 @@ const process = std.process;
 const mem = std.mem;
 const print = std.debug.print;
 
-const png_CHUNK_IHDR =  struct {
+const png_CHUNK_IHDR = extern struct {
     length: u32 align(1),
     chunk_type: u32 align(1),
     data: png_IHDR align(1),
     crc: u32 align(1),
 };
 
-const png_IHDR = struct {
+const png_IHDR = extern struct {
     width: u32 align(1),
     height: u32 align(1),
     bit_depth: u8,
@@ -41,13 +41,13 @@ pub fn main() !void {
 
     const data = try file.readToEndAlloc(allocator, 102400000);
 
-    if (mem.eql(u8, data[0..7], &[_]u8{137, 80, 78, 71, 13, 10, 26, 10})){
+    if (mem.eql(u8, data[0..7], &[_]u8{ 137, 80, 78, 71, 13, 10, 26, 10 })) {
         _ = try io.getStdErr().write("Not a PNG file");
         return;
     }
 
     var offset: usize = 8;
-    var iHDR = while(true) {
+    var iHDR = while (true) {
         // find the ihdr chunk
         var chunk: *png_CHUNK_IHDR = allocator.create(png_CHUNK_IHDR) catch unreachable;
         const chunk_size = @sizeOf(png_CHUNK_IHDR);
@@ -57,9 +57,9 @@ pub fn main() !void {
         chunk.data.width = @byteSwap(chunk.data.width);
         chunk.data.height = @byteSwap(chunk.data.height);
 
-        if (chunk.chunk_type == 0x52444849){
+        if (chunk.chunk_type == 0x52444849) {
             break chunk;
-        } else if(offset >= data.len){
+        } else if (offset >= data.len) {
             _ = try io.getStdErr().write("No IHDR chunk found");
             return;
         } else {
@@ -68,7 +68,6 @@ pub fn main() !void {
         }
     };
     defer allocator.destroy(iHDR);
-
 
     std.debug.print("{}", .{iHDR});
 }
